0210b2484549435477e8694addb3b0835e81f662
İbrahim <43969350+ipobeatz@users.noreply.github.com>
2023-06-19 22:54:51 +0300

    Update Program.cs

--- TelefonRehber/Program.cs
+++ TelefonRehber/Program.cs
@@ -1,22 +1,23 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace TelefonRehber
+namespace Telefon_Rehberi
 {
-    internal static class Program
+    static class Program
     {
         /// <summary>
-        /// Uygulamanın ana girdi noktası.
+        /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            Cls.SQLConnectionClass.Baglanti();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new KullaniciGiris());
         }
     }
 }
